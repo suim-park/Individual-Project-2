8a919b3257a135bd8f481c86a1800552fd0f08e8
Suim <143478016+suim-park@users.noreply.github.com>
2023-10-29 21:44:36 +0000

    Update test files

--- rust-cli-binary/tests/test.rs
+++ rust-cli-binary/tests/test.rs
@@ -21,7 +21,6 @@ mod tests {
 
         // 파일이 실제로 생성되었는지 확인합니다.
         assert!(fs::metadata(test_path).is_ok(), "Failed to create the file at {}", test_path);
-
     }
 
     #[test]
@@ -35,6 +34,21 @@ mod tests {
 
         // 결과가 Ok(())인지 확인합니다.
         assert!(result.is_ok(), "Transform function failed with {:?}", result);
+
+        // SQLite 데이터베이스에 연결하고 데이터를 검증합니다.
+        let conn = Connection::open_with_flags(db_path, OpenFlags::SQLITE_OPEN_READ_WRITE).unwrap();
+        let mut stmt = conn.prepare("SELECT year, month, passengers FROM data").unwrap();
+        let rows: Vec<(i32, String, i32)> = stmt.query_map([], |row| {
+            Ok((
+                row.get(0)?,
+                row.get(1)?,
+                row.get(2)?,
+            ))
+        }).unwrap().map(|row| row.unwrap()).collect();
+
+        assert_eq!(rows.len(), 2, "Unexpected number of rows in the database.");
+        assert_eq!(rows[0], (2023, "January".to_string(), 100));
+        assert_eq!(rows[1], (2023, "February".to_string(), 150));
     }
 
     #[test]
@@ -47,10 +61,8 @@ mod tests {
 
         // 생성된 데이터를 검증합니다.
         let conn = Connection::open(db_path).unwrap();
-        let passengers: i32 = conn.query_row("SELECT passengers FROM test_flightsDB WHERE year = 2023 AND month = 'March'", params![], |row| row.get(0)).unwrap();
+        let passengers: i32 = conn.query_row("SELECT passengers FROM data WHERE year = 2023 AND month = 'March'", params![], |row| row.get(0)).unwrap();
         assert_eq!(passengers, 200);
-
-        std::fs::remove_file(db_path).unwrap();
     }
 
     #[test]
@@ -63,8 +75,6 @@ mod tests {
         // 데이터베이스에서 데이터를 읽습니다.
         let result = read(db_path);
         assert!(result.is_ok(), "Read function failed with {:?}", result);
-
-        std::fs::remove_file(db_path).unwrap();
     }
 
     #[test]
@@ -81,8 +91,6 @@ mod tests {
         // 수정된 데이터를 검증합니다.
         let passengers: i32 = conn.query_row("SELECT passengers FROM data WHERE year = 2023 AND month = 'May'", params![], |row| row.get(0)).unwrap();
         assert_eq!(passengers, 350);
-
-        std::fs::remove_file(db_path).unwrap();
     }
 
     #[test]
@@ -99,7 +107,5 @@ mod tests {
         // 데이터가 정상적으로 삭제되었는지 검증합니다.
         let count: i32 = conn.query_row("SELECT COUNT(*) FROM data WHERE year = 2023", params![], |row| row.get(0)).unwrap();
         assert_eq!(count, 0);
-
-        std::fs::remove_file(db_path).unwrap();
     }
 }
