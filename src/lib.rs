bcb8bf6587023811ac3098b7b2edc5b72682aa5e
Suim <143478016+suim-park@users.noreply.github.com>
2023-10-29 18:24:50 +0000

    Update files

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::Write;
 use reqwest;
+use rusqlite::{params, Connection};
 
 pub fn extract(url: &str, file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
     // HTTP 요청을 통해 URL에서 데이터를 가져옵니다.
@@ -16,5 +17,27 @@ pub fn extract(url: &str, file_path: &str) -> Result<(), Box<dyn std::error::Err
     Ok(())
 }
 
-// 사용 예시:
-// download_to_csv("https://example.com/data.csv", "local_data.csv").unwrap();
+pub fn transform(csv_path: &str, db_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    // CSV 파일을 엽니다.
+    let mut rdr = csv::Reader::from_path(csv_path)?;
+
+    // SQLite 데이터베이스 파일을 생성하거나 연결합니다.
+    let conn = Connection::open(db_path)?;
+
+    // 적절한 테이블을 생성합니다. 이 예제에서는 단순한 구조로 가정합니다.
+    // 실제 사용 사례에 따라 테이블 구조를 조정할 필요가 있을 수 있습니다.
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS data (year INTEGER, month TEXT, passengers INTEGER)", // 컬럼 이름과 타입은 실제 CSV 구조에 맞게 조정하세요.
+        [],
+    )?;
+
+    for result  in rdr.deserialize() {
+        let (year, month, passengers): (i32, String, i32) = result?;  // 타입과 변수는 실제 CSV 구조에 따라 조정하세요.
+        conn.execute(
+            "INSERT INTO data (year, month, passengers) VALUES (?1, ?2, ?3)",
+            params![year, month, passengers],
+        )?;
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use rust_cli_binary::extract;
+use rust_cli_binary::transform;
 
 fn main() {
     extract("https://github.com/suim-park/Individual-Project-2/raw/main/flights.csv", "flights.csv").unwrap();
+    
+    let csv_path = "flights.csv";
+    let db_path = "flightsDB.db";
+    match transform(csv_path, db_path) {
+        Ok(_) => println!("CSV file has been successfully converted to SQLite DB."),
+        Err(e) => println!("An error occurred during conversion: {}", e),
+    }
 }
