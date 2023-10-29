abfbe6d0ff23c5c88456bf9092cca5b9fbdf3aa4
Suim <143478016+suim-park@users.noreply.github.com>
2023-10-29 19:06:53 +0000

    Update files

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::Write;
 use reqwest;
-use rusqlite::{params, Connection};
+use rusqlite::{params, Connection, Result};
 
 pub fn extract(url: &str, file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
     // HTTP 요청을 통해 URL에서 데이터를 가져옵니다.
@@ -38,6 +38,74 @@ pub fn transform(csv_path: &str, db_path: &str) -> Result<(), Box<dyn std::error
             params![year, month, passengers],
         )?;
     }
+
+    Ok(())
+}
+
+pub fn create(db_path: &str, year: i32, month: &str, passengers: i32) -> Result<(), Box<dyn std::error::Error>> {
+    // SQLite 데이터베이스 파일에 연결합니다.
+    let conn = rusqlite::Connection::open(db_path)?;
+
+    // 데이터를 `data` 테이블에 삽입합니다.
+    conn.execute(
+        "INSERT INTO data (year, month, passengers) VALUES (?1, ?2, ?3)",
+        params![year, month, passengers],
+    )?;
+
+    println!("Data successfully created into the database.");
+
+    Ok(())
+}
+
+pub fn read(db_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    // SQLite 데이터베이스 파일에 연결합니다.
+    let conn = rusqlite::Connection::open(db_path)?;
+
+    // 쿼리를 실행하여 결과를 가져옵니다.
+    let mut stmt = conn.prepare("SELECT year, month, passengers FROM data")?;
+    let rows = stmt.query_map([], |row| {
+        Ok((
+            row.get::<_, i32>("year")?,
+            row.get::<_, String>("month")?,
+            row.get::<_, i32>("passengers")?,
+        ))
+    })?;
+
+    // 결과를 출력합니다.
+    for row_result in rows {
+        let (year, month, passengers) = row_result?;
+        println!("{} - {}: {}", year, month, passengers);
+    }
+
+    Ok(())
+}
+
+pub fn update(db_path: &str, year: i32, month: &str, passengers: i32) -> Result<(), Box<dyn std::error::Error>> {
+    // SQLite 데이터베이스 파일에 연결합니다.
+    let conn = rusqlite::Connection::open(db_path)?;
+
+    // 지정된 연도와 월에 해당하는 데이터의 승객 수를 변경합니다.
+    let rows_modified = conn.execute(
+        "UPDATE data SET passengers = ?3 WHERE year = ?1 AND month = ?2",
+        params![year, month, passengers],
+    )?;
+
+    if rows_modified == 0 {
+        println!("No data found for the specified year and month.");
+    } else {
+        println!("Data successfully updated in the database.");
+    }
+
+    Ok(())
+}
+
+pub fn delete(db_path: &str, year: i32) -> Result<()> {
+    let conn = Connection::open(db_path)?;
+
+    conn.execute(
+        "DELETE FROM data WHERE year = ?1",
+        params![year],
+    )?;
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use rust_cli_binary::extract;
 use rust_cli_binary::transform;
+use rust_cli_binary::create;
+use rust_cli_binary::read;
+use rust_cli_binary::update;
+use rust_cli_binary::delete;
 
 fn main() {
     extract("https://github.com/suim-park/Individual-Project-2/raw/main/flights.csv", "flights.csv").unwrap();
@@ -9,5 +13,37 @@ fn main() {
     match transform(csv_path, db_path) {
         Ok(_) => println!("CSV file has been successfully converted to SQLite DB."),
         Err(e) => println!("An error occurred during conversion: {}", e),
+    }
+
+    match create("flightsDB.db", 2023, "October", 800) {
+        Ok(_) => println!("Successfully inserted data into the SQLite DB."),
+        Err(e) => println!("Error occurred while inserting data: {}", e),
+    }
+
+   
+    match read("flightsDB.db") {
+        Ok(_) => println!("Successfully read from the SQLite DB."),
+        Err(e) => println!("Error occurred while reading data: {}", e),
+    }
+  
+    match update("flightsDB.db", 2023, "October", 1000) {
+        Ok(_) => println!("Successfully updated data in the SQLite DB."),
+        Err(e) => println!("Error occurred while updating data: {}", e),
+    }
+    
+    match read("flightsDB.db") {
+        Ok(_) => println!("Successfully read from the SQLite DB."),
+        Err(e) => println!("Error occurred while reading data: {}", e),
+    }
+
+    let year_to_delete = 2023;
+    match delete(db_path, year_to_delete) {
+        Ok(_) => println!("Successfully deleted data for year {}.", year_to_delete),
+        Err(e) => println!("An error occurred: {}", e),
+    }
+
+    match read("flightsDB.db") {
+        Ok(_) => println!("Successfully read from the SQLite DB."),
+        Err(e) => println!("Error occurred while reading data: {}", e),
     }
 }
